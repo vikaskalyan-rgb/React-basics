56c81c93fbdcdd81f5958461362e4e141f826581
Vikas Kalyan <64362404+vikaskalyan-rgb@users.noreply.github.com>
2022-05-11 01:35:09 +0530

    Add files via upload

---
+++ Boot.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Card } from 'react-bootstrap';
+
+const Func=()=>{
+    return(
+        <div>
+             <Card>
+    <Card.Img variant="top" src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fwallpaperaccess.com%2Ffull%2F5991874.jpg&imgrefurl=https%3A%2F%2Fwallpaperaccess.com%2Flara-croft-desktop&tbnid=3XgtelCb_r9fJM&vet=12ahUKEwjnuo-V2tX3AhVrjtgFHU3GD9AQMygCegUIARC5AQ..i&docid=fnlX7yFL7HrlpM&w=3840&h=2400&q=lara%20croft%20hd%20wallpaper&ved=2ahUKEwjnuo-V2tX3AhVrjtgFHU3GD9AQMygCegUIARC5AQ" />
+    <Card.Body>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the bulk
+        of the card's content.
+      </Card.Text>
+    </Card.Body>
+  </Card>
+  <br />
+  <Card>
+    <Card.Body>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the bulk
+        of the card's content.
+      </Card.Text>
+    </Card.Body>
+    <Card.Img variant="bottom" src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages5.alphacoders.com%2F388%2F388250.png&imgrefurl=https%3A%2F%2Fwall.alphacoders.com%2Ftag%2Flara-croft-wallpapers&tbnid=eRbPwxeXJY2FzM&vet=12ahUKEwjnuo-V2tX3AhVrjtgFHU3GD9AQMygKegUIARDJAQ..i&docid=H4REL9KFP1W3eM&w=3327&h=1590&q=lara%20croft%20hd%20wallpaper&ved=2ahUKEwjnuo-V2tX3AhVrjtgFHU3GD9AQMygKegUIARDJAQ" />
+  </Card>
+        </div>
+    )
+}
+export default Func;
